f7f746526bc9f1f1574a72a31c7c0478b4dd0076
kapillohanii <kapillohani023@gmail.com>
2024-04-27 17:35:08 +0530

    refactored subgrid

--- components/subgrid/Subgrid.tsx
+++ components/subgrid/Subgrid.tsx
@@ -2,27 +2,34 @@
 
 import { SimpleGrid, Skeleton, Container, Stack, useMantineTheme, px } from '@mantine/core';
 
-const getChild = (height: number) => <Skeleton height={height} radius="md" animate={false} />;
-const BASE_HEIGHT = 360;
+const getChild = (width: number, height: number) => <Skeleton height={height} width={width} radius="md" animate={false} />;
+const BASE_WIDTH = 260
+const BASE_HEIGHT = 600;
 const getSubHeight = (children: number, spacing: number) =>
   BASE_HEIGHT / children - spacing * ((children - 1) / children);
 
 export function Subgrid() {
   const theme = useMantineTheme();
   return (
-    <Container my="md">
-      <SimpleGrid cols={{ base: 1, xs: 4 }}>
-        {getChild(BASE_HEIGHT)}
+    <Container my="xl" size="xl">
+      <SimpleGrid cols={{ base: 1, xs: 4 }} spacing="md">
         <Stack>
-          {getChild(getSubHeight(2, px(theme.spacing.md) as number))}
-          {getChild(getSubHeight(2, px(theme.spacing.md) as number))}
+          {getChild(BASE_WIDTH, BASE_HEIGHT - 450)}
+          {getChild(BASE_WIDTH, BASE_HEIGHT - 165)}
         </Stack>
         <Stack>
-          {getChild(getSubHeight(3, px(theme.spacing.md) as number))}
-          {getChild(getSubHeight(3, px(theme.spacing.md) as number))}
-          {getChild(getSubHeight(3, px(theme.spacing.md) as number))}
+          {getChild(BASE_WIDTH, getSubHeight(2, px(theme.spacing.md) as number))}
+          {getChild(BASE_WIDTH, getSubHeight(2, px(theme.spacing.md) as number))}
         </Stack>
-        {getChild(BASE_HEIGHT)}
+        <Stack>
+          {getChild(BASE_WIDTH, getSubHeight(3, px(theme.spacing.md) as number))}
+          {getChild(BASE_WIDTH, getSubHeight(3, px(theme.spacing.md) as number))}
+          {getChild(BASE_WIDTH, getSubHeight(3, px(theme.spacing.md) as number))}
+        </Stack>
+        <Stack>
+          {getChild(BASE_WIDTH, BASE_HEIGHT - 165)}
+          {getChild(BASE_WIDTH, BASE_HEIGHT - 450)}
+        </Stack>
       </SimpleGrid>
     </Container>
   );
